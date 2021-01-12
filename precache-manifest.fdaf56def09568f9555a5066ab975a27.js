9a4f1e00a7f2a5b3762d1c49e1eb4e5a5ad96be6
Michał Płużek <michalpluzek@gmail.com>
2021-01-12 21:34:44 +0100

    Updates

---
+++ precache-manifest.fdaf56def09568f9555a5066ab975a27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec7f778b5d14e65a73dd",
+    "url": "/app-rr/static/css/main.b8af4ef9.chunk.css"
+  },
+  {
+    "revision": "ec7f778b5d14e65a73dd",
+    "url": "/app-rr/static/js/main.c534e713.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "a2c2c5442be9c0f57a6c",
+    "url": "/app-rr/static/js/2.b9dd40e9.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "75c66f3bcc0c6cd460c2624b051e2f46",
+    "url": "/app-rr/index.html"
+  }
+];
